02703a8e57062d2661512b7072d9571456038857
Figi8Cake <87722837+Figi8Cake@users.noreply.github.com>
2022-02-24 07:17:09 -0800

    Fixed

--- js/sound.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    var popupsound = document.getElementById("notifypop");
-
-    function autoNotify() {
-        popupsound.play(); //play the audio file
-    }
-});
