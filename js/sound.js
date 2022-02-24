df66a865e443074edf3895a3d1291535481fdc6d
Figi8Cake <87722837+Figi8Cake@users.noreply.github.com>
2022-02-24 07:01:30 -0800

    Added Rickroll

---
+++ js/sound.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var popupsound = document.getElementById("notifypop");
+
+    function autoNotify() {
+        popupsound.play(); //play the audio file
+    }
+});
